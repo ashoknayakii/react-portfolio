6162a17c27e88549668b897ffa25fe6da53c92b1
Ashok Nayak <ashoknayak@MacBook-Air.nyc.rr.com>
2022-05-14 13:42:06 -0400

    starter files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
+  const [categories] = useState([
+
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  console.log('currentCategory', currentCategory)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <div>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </div>
+      </main>
     </div>
   );
 }
