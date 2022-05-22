7bae0886a1f2d8767403f15f08e590e28abcd5b2
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-22 14:07:12 -0400

    CTA added

---
+++ src/components/Header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com" target="_blank"></a>
+        <a href="https://twitter.com" target="_blank"></a>
+      
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/images/portfolioprofile.jpeg'
 
 const header = () => {
   return (
@@ -8,6 +10,13 @@ const header = () => {
       <h5>Hello I'm</h5>
       <h1>Ashok Nayak</h1>
       <h5 classname="text-light">Fullstack Developer</h5>
+      <CTA />
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+
+      <a href="#contact" className='scroll_down'>Scroll Down</a>
       </div>
     </header>
 
