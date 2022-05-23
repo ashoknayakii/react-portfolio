1c0d902ba8bc28059124ad3b89ad067240c10840
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-23 17:51:01 -0400

    about section in progress

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import './About.css'
+import Random from '../../assets/images/feetatmontauk.jpeg'
+import {BiAward} from 'react-icons/bi'
 
 const About = () => {
   return (
@@ -9,16 +12,41 @@ const About = () => {
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me-image">
-            <img src="" alt="" />
+            <img src={Random} alt="Feet at Montauk" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <BiAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>N/A</small>
+            </article>
+
+            <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <BiAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>N/A</small>
+            </article>
+
+            <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <BiAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>N/A</small>
+            </article>
+
           </div>
         </div>
       </div>
-      
     </section>
   )
 }
 
 export default About
 
-        {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
-        // <p>Ashok Nayak is a full stack developer with a background in music and hospitality.  Prior to his entry into web development, he spent his time writing, collaborating and performing original music as well as working at some of the top restaurants in NYC.  He brings a diverse array of skills with a focus on developing tools and digital infrastructure to support artists in the music industry. </p>
+        {/* // <p>Ashok Nayak is a full stack developer with a background in music and hospitality.  Prior to his entry into web development, he spent his time writing, collaborating and performing original music as well as working at some of the top restaurants in NYC.  He brings a diverse array of skills with a focus on developing tools and digital infrastructure to support artists in the music industry. </p> */}
