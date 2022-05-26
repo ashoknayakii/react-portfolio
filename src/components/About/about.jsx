6ef2c2105e6de7ee90b0df20184750a5cf04fc3c
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-26 00:12:28 -0400

    about component and styling finished

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -2,45 +2,42 @@ import React from 'react'
 import './About.css'
 import Random from '../../assets/images/feetatmontauk.jpeg'
 import {BiAward} from 'react-icons/bi'
-
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+ 
 const About = () => {
   return (
     <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
-      <div className="container about_container">
-        <div className="about_me">
-          <div className="about_me-image">
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
             <img src={Random} alt="Feet at Montauk" />
           </div>
         </div>
 
-        <div className="about_content">
-          <div className="about_cards">
-            <article className="about_card">
-              <BiAward className='about_icon' />
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <BiAward className='about__icon' />
               <h5>Experience</h5>
               <small>N/A</small>
             </article>
-
-            <div className="about_content">
-          <div className="about_cards">
-            <article className="about_card">
-              <BiAward className='about_icon' />
-              <h5>Experience</h5>
+            <article className="about__card">
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ clients worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
               <small>N/A</small>
             </article>
-
-            <div className="about_content">
-          <div className="about_cards">
-            <article className="about_card">
-              <BiAward className='about_icon' />
-              <h5>Experience</h5>
-              <small>N/A</small>
-            </article>
-
           </div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde sequi earum in facilis eos quibusdam, placeat mollitia ad debitis, excepturi rerum consequuntur distinctio pariatur praesentium repudiandae natus perspiciatis dolores omnis!</p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
     </section>
